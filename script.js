7f34f3c0ae103908e947894b1326915d19e5317b
RetroOlympics <145793523+RetroOlympics@users.noreply.github.com>
2023-10-22 21:45:06 +0200

    2210
    Build 22/10

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
+
+/* JS FOR FAQ */
+
+$('body').delegate('.c-faq', 'click', function(){
+    $('.c-faq').removeClass('c-faq--active');
+    $(this).addClass('c-faq--active');
+  });

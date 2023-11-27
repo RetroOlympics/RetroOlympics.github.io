42f30a8be52f9f482f609e8c588e9ea01aa39276
MonkeyBug <136386334+monkey-bug@users.noreply.github.com>
2023-11-26 18:23:23 -0600

    rework mobile navbar to not need JS

--- js/navlist.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('#menu-icon');
-const navlist = document.querySelector('header nav');
-
-menu.addEventListener("click", () => {
-    navlist.classList.toggle('open');
-});

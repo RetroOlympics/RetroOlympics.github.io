d578f7b6e2849545057d14d30727baf86889ccec
MonkeyBug <136386334+monkey-bug@users.noreply.github.com>
2023-11-28 15:28:46 -0600

    move volunteer JS to file

---
+++ js/volunteers.js
@@ -0,0 +1,61 @@
+const volunteers = {
+    "voiceofautumn": "Host & Jack-Of-All-Trades",
+    "Searo": "Co-Host & Leaderboard Developer",
+    "starblades": "Shoutcaster & Media",
+    "StingX2": "Shoutcaster",
+    "Taysuu": "Shoutcaster",
+    "TimeCrush": "Shoutcaster",
+    "Wimpyfox": "Shoutcaster",
+    "wolfman2000": "Shoutcaster",
+    "Pampa50": "Referee",
+    "soltyd": "Referee",
+    "Bartis1989": "Leaderboard Developer",
+    "Bryan1150": "Leaderboard Developer",
+    "clymax": "Leaderboard & Web Developer",
+    "drisc": "Web Developer",
+    "MonkeyBug": "Web Developer",
+    "Nabas6545": "Media Producer",
+    "Tayadaoc": "Media Producer",
+};
+
+const container = document.getElementById("volunteers-container");
+const wheel = document.createElement("div");
+wheel.classList.add("wheel");
+wheel.style.setProperty("--n", Object.keys(volunteers).length.toString());
+
+let active = null;
+
+for (const [index, [user, desc]] of Object.entries(Object.entries(volunteers))) {
+    const icon = document.createElement("div");
+    icon.classList.add("icon");
+    icon.style.setProperty("--i", index.toString());
+    icon.innerHTML = `<img src="https://media.retroachievements.org/UserPic/${user}.png">`;
+
+    const card = document.createElement("div")
+    card.classList.add("card");
+    card.innerHTML = `
+        <ra-userpic>${user}</ra-userpic>
+        <div class="text">
+            <a href="https://retroachievements.org/user/${user}">${user}</a>
+            ${desc}
+        </div>
+    `;
+
+    icon.firstChild.addEventListener("mouseenter", () => {
+        if (active !== null) {
+            active.classList.remove("active");
+        }
+        card.classList.add("active");
+        active = card;
+    });
+
+    wheel.append(icon, card);
+}
+
+container.appendChild(wheel);
+
+// randomizer
+const n = Math.floor(Math.random() * Object.keys(volunteers).length);
+const card = document.querySelectorAll(".card")[n];
+card.classList.add("active");
+active = card;

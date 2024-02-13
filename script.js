87dde6ef9a26fdd5ec1d411148128b6fdc514f8b
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2024-02-13 13:45:21 -0600

    replace scrollreveal with css animations

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-sr.reveal('.hero-text',{delay:200, origin:'top'});
-sr.reveal('.hero-img',{delay:450, origin:'top'});
-sr.reveal('.scroll-down',{delay:500, origin:'bottom'});
-
 /* JS FOR FAQ */
 
 $('body').delegate('.c-faq', 'click', function(){

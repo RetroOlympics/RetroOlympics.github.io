c95bae1c4a1f054775fc46ce4f3c6faca410d2c5
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2024-04-28 15:22:11 -0500

    update volunteers page

--- olympics/js/volunteers.js
+++ olympics/js/volunteers.js
@@ -1,66 +1,64 @@
 const volunteers = {
-    "voiceofautumn": "Host & Jack-Of-All-Trades",
-    "Searo": "Co-Host & Leaderboard Developer",
-    "starblades": "Shoutcaster & Media",
-    "StingX2": "Shoutcaster",
-    "TimeCrush": "Shoutcaster",
-    "Wimpyfox": "Shoutcaster",
-    "wolfman2000": "Shoutcaster",
-    "RaphMec": "Shoutcaster",
-    "freezestar": "Shoutcaster",
-    "Pampa50": "Referee",
-    "soltyd": "Referee",
-    "Amir96lx": "Referee",
-    "coolscribble": "Referee",
-    "Nickyy": "Referee",
-    "s0uth": "Referee",
-    "Bartis1989": "Leaderboard Developer",
-    "Bryan1150": "Leaderboard Developer",
-    "clymax": "Leaderboard & Web Developer",
-    "drisc": "Web Developer",
-    "MonkeyBug": "Web Developer",
-    "Nabas6545": "Media Producer",
-    "Tayadaoc": "Media Producer & Referee",
+    "Hosts": [
+        "voiceofautumn",
+        "Searo",
+    ],
+    "Stream Team": [
+        "starblades",
+        "TimeCrush",
+        "RaphMec",
+        "Wimpyfox",
+        "freezestar",
+        "Prota",
+        "AmazingBaha",
+        "theelkspeaks",
+        "Homuki",
+    ],
+    "Referee Team": [
+        "Amir96lx",
+        "Kiutgh",
+        "xClawz",
+        "Retrokaiser",
+    ],
+    "Game Testers": [
+        "Adenothe",
+        "GalacticSpear",
+        "Nickyy",
+        "r0ach3d",
+        "Tayadaoc",
+        "Zexerous",
+    ],
+    "Leaderboard Devs": [
+        "Bartis1989",
+        "clymax",
+        "s0uth",
+        "kmpers",
+    ],
+    "Web Team": [
+        "drisc",
+        "MonkeyBug",
+        "Daisey",
+    ],
+    "Media Team": [
+        "PrinnyPrinny",
+        "ChocoMilk",
+        "Nydaxn",
+        "TheoVellum",
+    ],
+    "Event Support": [
+        "Zegjita",
+    ],
 };
 
 const container = document.getElementById("volunteers-container");
-const wheel = document.createElement("div");
-wheel.classList.add("wheel");
-wheel.style.setProperty("--n", Object.keys(volunteers).length.toString());
+let output = "";
 
-let active = null;
-
-for (const [index, [user, desc]] of Object.entries(Object.entries(volunteers))) {
-    const icon = document.createElement("div");
-    icon.classList.add("icon");
-    icon.style.setProperty("--i", index.toString());
-    icon.innerHTML = `<img src="https://media.retroachievements.org/UserPic/${user}.png">`;
-
-    const card = document.createElement("div")
-    card.classList.add("card");
-    card.innerHTML = `
-        <ra-userpic>${user}</ra-userpic>
-        <div class="text">
-            <a href="https://retroachievements.org/user/${user}">${user}</a>
-            ${desc}
-        </div>
-    `;
-
-    icon.firstChild.addEventListener("mouseenter", () => {
-        if (active !== null) {
-            active.classList.remove("active");
-        }
-        card.classList.add("active");
-        active = card;
-    });
-
-    wheel.append(icon, card);
+for (const [team, members] of Object.entries(volunteers)) {
+    output += `<div><h1>${team}</h1><hr><div class="memberlist">`;
+    for (const member of members) {
+        output += `<ra-userpic>${member}</ra-userpic><p>${member}</p>`;
+    }
+    output += `</div></div>`;
 }
 
-container.appendChild(wheel);
-
-// randomizer
-const n = Math.floor(Math.random() * Object.keys(volunteers).length);
-const card = document.querySelectorAll(".card")[n];
-card.classList.add("active");
-active = card;
+container.innerHTML = output;
